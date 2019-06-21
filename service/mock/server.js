d56337d781efdae991d5408a9cb9e21c4c5f3899
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2019-06-20 23:47:53 -0700

    [rwp] basic executor/service interactions

--- service/mock/server.js
+++ service/mock/server.js
@@ -3,12 +3,14 @@ const http = require('http');
 const port = 3030;
 
 
-const content = fs.readFileSync('service/mock/mock_task.json', 'utf8');
+const config = JSON.parse(fs.readFileSync('service/mock/mock_task.json', 'utf8'));
+const src = fs.readFileSync('service/mock/mock_src.asm', 'utf8');
+config["source"] = src;
 
 const handler = (request, response) => {
   console.log(request.url);
   if (request.url == '/task') {
-    response.end(content);
+    response.end(JSON.stringify(config));
   } else {
     response.end();
   }
