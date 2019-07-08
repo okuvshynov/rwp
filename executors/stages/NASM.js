c9612fbb59e70471ae31030e5524809b149ecf33
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2019-07-07 23:40:58 -0700

    [rwp] cleanup + stages pipeline

---
+++ executors/stages/NASM.js
@@ -0,0 +1,10 @@
+// MIT License
+// Refer to /LICENSE file for full text
+// Copyright (c) 2019 Oleksandr Kuvshynov
+
+// IN: asm source code
+// IN: options
+// OUT: obj file
+// OUT: logs/info
+// OUT: warnings
+// OUT: errors
